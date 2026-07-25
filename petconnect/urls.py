e55082d25469e036f45e876fcb7a503f7aa4894a
Mona-Alakhras <monaalakhras00@gmail.com>
2026-07-25 14:15:34 +0300

    update urls

--- petconnect/urls.py
+++ petconnect/urls.py
@@ -21,7 +21,7 @@ from django.conf.urls.static import static
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('petcare/', include('petcare.urls')),
+    path('', include('petcare.urls')),
 ]
 if settings.DEBUG:
     urlpatterns += static(settings.MEDIA_URL, document_root=settings.MEDIA_ROOT)
